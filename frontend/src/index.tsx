0dd84e70a7629690572dc1d83767a43b3cbfe08a
草をこして森 <kyou0317kyou0317@gmail.com>
2025-01-12 18:47:06 +0900

    gitの動作テスト

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <h1 className="text-3xl font-bold text-blue-500">
-        Hello, Tailwind CSS!!!
+        Hello, Tailwind CSS!!!!!
       </h1>
     </div>
   );
