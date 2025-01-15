7e24f7684de9f93a619a189c4ce90ecef1f77816
草をこして森 <kyou0317kyou0317@gmail.com>
2025-01-15 11:21:12 +0900

    ボタン再利用可能

--- frontend/app/components/elements/button/Button.tsx
+++ frontend/app/components/elements/button/Button.tsx
@@ -1,25 +1,45 @@
 "use client";
+
 interface ButtonProps {
   text: string;
+  subText1: string;
+  subText2: string;
+  bgColor: string;
+  subTextColor: string;
+  onClick?: () => void;
 }
 
-const Button = ({ text }: ButtonProps) => {
+const Button = ({
+  text,
+  subText1,
+  subText2,
+  bgColor,
+  subTextColor,
+  onClick,
+}: ButtonProps) => {
   const handleClick = () => {
-    console.log("ボタンが押されました");
+    console.log(`${text}ボタンが押されました`);
+    if (onClick) {
+      onClick();
+    }
   };
+
   return (
     <div
       onClick={handleClick}
       role="button"
-      className="w-2/3 h-36 bg-sky-300 text-white flex items-start justify-center rounded-lg cursor-pointer border-2 blue-text-outline border-blue-900 focus:outline-none focus:ring-2 relative"
+      className={`w-2/3 h-36 ${bgColor} text-white flex items-start justify-center rounded-lg cursor-pointer border-2 blue-text-outline border-blue-900 focus:outline-none focus:ring-2 relative`}
     >
       <span className="text-4xl mt-4 font-bold">{text}</span>
-      {/* ⤴レスポンシブ対応させる */}
-      <div className="absolute bottom-1/4 w-[98%] h-1/4 bg-blue-500 text-white flex items-center justify-end pr-4 cursor-pointer">
-        一人の方はこちら
+      <div
+        className={`absolute bottom-1/4 w-[98%] h-1/4 ${subTextColor} text-white flex items-center justify-end pr-4 cursor-pointer`}
+      >
+        {subText1}
       </div>
-      <div className="absolute bottom-0 w-[98%] h-1/4 bg-sky-300 text-white flex items-center justify-center rounded-lg cursor-pointer text-2xl font-bold ">
-        ネットを使って世界中の人と対戦
+      <div
+        className={`absolute bottom-0 w-[98%] h-1/4 ${bgColor} text-white flex items-center justify-center rounded-lg cursor-pointer text-2xl font-bold`}
+      >
+        {subText2}
       </div>
     </div>
   );
---
+++ frontend/features/SelectGame/TitleRPS.tsx
@@ -0,0 +1,5 @@
+const TitleRPS = () => {
+  return <div>特殊カードジャンケン</div>;
+};
+
+export default TitleRPS;
