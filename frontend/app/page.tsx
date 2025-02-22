5ffa0cd67856161ee3aed472895a4df5fe34cbc6
草をこして森 <kyou0317kyou0317@gmail.com>
2025-02-23 01:49:57 +0900

    スタート画面作成

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,26 @@
-import Link from "next/link";
+"use client";
+
+import { useRouter } from "next/navigation";
+import Button from "./components/elements/button/Button";
+import TitleRPS from "./components/features/SelectGame/TitleRPS";
 
 const Page = () => {
+  const router = useRouter();
+  const moveGameSelect = () => {
+    router.push("selectGame");
+  };
   return (
     <div className="flex justify-center items-center flex-col">
-      <p>Appの要素</p>
-      <Link href="/selectGame">About</Link>
+      <TitleRPS />
+      <Button
+        text="ゲームスタート"
+        subText1="ゲームセレクト画面に移動します"
+        subText2="まずはここをクリック"
+        bgColor="bg-lime-300"
+        subTextColor="bg-green-400"
+        addClass="my-16"
+        onClick={moveGameSelect}
+      />
     </div>
   );
 };
