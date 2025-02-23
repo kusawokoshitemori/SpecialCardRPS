9f5b2e204541707a9db0d37e1df69f597b3c80fd
草をこして森 <kyou0317kyou0317@gmail.com>
2025-02-23 20:09:42 +0900

    vercel.jsonの追加

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["http://192.168.0.30:3000"],
   },
 });
 
