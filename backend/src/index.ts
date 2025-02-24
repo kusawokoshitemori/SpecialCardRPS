486116b23d7d41dae9d7f9f34e28901c2741b9d8
草をこして森 <kyou0317kyou0317@gmail.com>
2025-02-24 18:49:13 +0900

    適切ねURLに変更

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: ["http://192.168.0.30:3000"],
+    origin: ["https://special-card-rps-jet.vercel.app/"],
   },
 });
 
