4a020f8904da8ebca61ecfb0adf253737877864c
草をこして森 <kyou0317kyou0317@gmail.com>
2025-02-24 18:51:47 +0900

    適切なURLに変更

--- frontend/app/components/contexts/socketContext.tsx
+++ frontend/app/components/contexts/socketContext.tsx
@@ -21,7 +21,7 @@ export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
   const [isMatched, setIsMatched] = useState(false);
 
   useEffect(() => {
-    const newSocket = io("http://localhost:4000", {
+    const newSocket = io("https://render.com/docs/web-services#port-binding", {
       autoConnect: true,
       reconnection: true,
     });
